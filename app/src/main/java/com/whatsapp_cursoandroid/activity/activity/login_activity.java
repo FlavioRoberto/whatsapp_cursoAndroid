16f0a425dba92a9c01013c4e7a8fad1fe65daf93
FlavioRoberto <flaviolive3@hotmail.com>
2017-08-04 13:29:47 -0300

    Activity cadastro

--- app/src/main/java/com/whatsapp_cursoandroid/activity/activity/login_activity.java
+++ app/src/main/java/com/whatsapp_cursoandroid/activity/activity/login_activity.java
@@ -1,15 +1,24 @@
 package com.whatsapp_cursoandroid.activity.activity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import com.whatsapp_cursoandroid.R;
 
 public class login_activity extends AppCompatActivity {
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    public void redirecionaCadastro(View view){
+        Intent intent = new Intent(this,CadastroActivity.class);
+        startActivity(intent);
     }
 }
