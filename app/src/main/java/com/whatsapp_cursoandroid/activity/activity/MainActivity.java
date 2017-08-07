288b2d1d45737d8ce6aaaf5e5d967bea9cb1c2c7
FlavioRoberto <flaviolive3@hotmail.com>
2017-08-07 14:12:14 -0300

    Toolbar pronta

--- app/src/main/java/com/whatsapp_cursoandroid/activity/activity/MainActivity.java
+++ app/src/main/java/com/whatsapp_cursoandroid/activity/activity/MainActivity.java
@@ -2,6 +2,10 @@ package com.whatsapp_cursoandroid.activity.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
@@ -13,21 +17,31 @@ public class MainActivity extends AppCompatActivity {
 
     private Button btnDeslogar;
     private FirebaseAuth auth;
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        btnDeslogar = (Button)findViewById(R.id.BtnDeslogar);
 
+        //invoca componentes
         auth = ConfiguracaoFirebase.getFirebaseAuth();
+        toolbar = (Toolbar)findViewById(R.id.ToolbarId);
 
-        btnDeslogar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                auth.signOut();
-            }
-        });
+        //preparando Toolbar
+        toolbar.setTitle("Whatsapp");
+        setSupportActionBar(toolbar);
+    }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_toolbar,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return super.onOptionsItemSelected(item);
     }
 }
